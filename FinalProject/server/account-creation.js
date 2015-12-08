866905cef722271a22cc6b3680d938e766ddc6be
leonzl <leonz@hawaii.edu>
2015-12-08 13:13:43 -1000

    Revert "Main Chatroom"
    This reverts commit 3cedc7117813cc0250ec66e1bec10e27bc130f68.

--- FinalProject/server/account-creation.js
+++
@@ -1,8 +0,0 @@
-if(Meteor.isServer){
-    Accounts.onCreateUser(function(options, user){
-        user.roles = ["member"];
-        if (options.profile){
-            user.profile = options.profile;}
-        return user;
-    });
-}
